78bf206a2d1301a59a053c9b3de895429e3aaec8
DevExpressExampleBot <fakeEmail>
2023-07-24 21:26:41 +0400

    Source auto update [skip ci]

--- VB/CustomTextEdit/CustomEdit.vb
+++ VB/CustomTextEdit/CustomEdit.vb
@@ -1,30 +1,29 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.XtraEditors
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 
 Namespace CustomTextEdit
-	Public Class MyCustomTextEdit
-		Inherits TextEdit
-		Shared Sub New()
-			RepositoryItemCustomTextEdit.Register()
-		End Sub
 
-		Public Sub New()
-		End Sub
+    Public Class MyCustomTextEdit
+        Inherits TextEdit
 
-		Public Overrides ReadOnly Property EditorTypeName() As String
-			Get
-				Return RepositoryItemCustomTextEdit.CustomTextEditName
-			End Get
-		End Property
+        Shared Sub New()
+            RepositoryItemCustomTextEdit.Register()
+        End Sub
 
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
-		Public Shadows ReadOnly Property Properties() As RepositoryItemCustomTextEdit
-			Get
-				Return TryCast(MyBase.Properties, RepositoryItemCustomTextEdit)
-			End Get
-		End Property
-	End Class
+        Public Sub New()
+        End Sub
+
+        Public Overrides ReadOnly Property EditorTypeName As String
+            Get
+                Return RepositoryItemCustomTextEdit.CustomTextEditName
+            End Get
+        End Property
+
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)>
+        Public Overloads ReadOnly Property Properties As RepositoryItemCustomTextEdit
+            Get
+                Return TryCast(MyBase.Properties, RepositoryItemCustomTextEdit)
+            End Get
+        End Property
+    End Class
 End Namespace
--- VB/CustomTextEdit/RepositoryItemCustomTextEdit.vb
+++ VB/CustomTextEdit/RepositoryItemCustomTextEdit.vb
@@ -1,76 +1,80 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.XtraEditors.Drawing
 Imports DevExpress.XtraEditors.Registrator
 Imports DevExpress.XtraEditors.Repository
 Imports DevExpress.XtraEditors.ViewInfo
 Imports System
-Imports System.Collections.Generic
 Imports System.Runtime.InteropServices
 
 Namespace CustomTextEdit
-	<UserRepositoryItem("Register")> _
-	Public Class RepositoryItemCustomTextEdit
-		Inherits RepositoryItemTextEdit
-		Private _nullValuePromptAlwaysVisible As Boolean
-		Public Property NullValuePromptAlwaysVisible() As Boolean
-			Get
-				Return _nullValuePromptAlwaysVisible
-			End Get
-			Set(ByVal value As Boolean)
-				_nullValuePromptAlwaysVisible = value
-				ShowNullPrompt()
-			End Set
-		End Property
-		Public Overrides Property NullValuePrompt() As String
-			Get
-				Return MyBase.NullValuePrompt
-			End Get
-			Set(ByVal value As String)
-				MyBase.NullValuePrompt = value
-				ShowNullPrompt()
-			End Set
-		End Property
-		Public Const CustomTextEditName As String = "CustomTextEdit"
 
-		Shared Sub New()
-			Register()
-		End Sub
-		Public Sub New()
-		End Sub
+    <UserRepositoryItem("Register")>
+    Public Class RepositoryItemCustomTextEdit
+        Inherits RepositoryItemTextEdit
 
-		Public Overrides ReadOnly Property EditorTypeName() As String
-			Get
-				Return CustomTextEditName
-			End Get
-		End Property
+        Private _nullValuePromptAlwaysVisible As Boolean
 
-		Public Overrides Sub Assign(ByVal item As RepositoryItem)
-			BeginUpdate()
-			Try
-				MyBase.Assign(item)
-				Dim source As RepositoryItemCustomTextEdit = TryCast(item, RepositoryItemCustomTextEdit)
-				If source Is Nothing Then
-					Return
-				End If
-				NullValuePromptAlwaysVisible = source.NullValuePromptAlwaysVisible
-			Finally
-				EndUpdate()
-			End Try
-		End Sub
+        Public Property NullValuePromptAlwaysVisible As Boolean
+            Get
+                Return _nullValuePromptAlwaysVisible
+            End Get
 
-		Public Shared Sub Register()
-			EditorRegistrationInfo.Default.Editors.Add(New EditorClassInfo(CustomTextEditName, GetType(MyCustomTextEdit), GetType(RepositoryItemCustomTextEdit), GetType(TextEditViewInfo), New TextEditPainter(), True))
-		End Sub
+            Set(ByVal value As Boolean)
+                _nullValuePromptAlwaysVisible = value
+                ShowNullPrompt()
+            End Set
+        End Property
 
-		Private Sub ShowNullPrompt()
-			Dim behaviour As Integer = Convert.ToInt32(NullValuePromptAlwaysVisible)
-			If OwnerEdit IsNot Nothing AndAlso OwnerEdit.IsHandleCreated AndAlso (Not String.IsNullOrEmpty(NullValuePrompt)) Then
-				SendMessage(OwnerEdit.MaskBox.Handle, &H1501, New IntPtr(behaviour), NullValuePrompt)
-			End If
-		End Sub
+        Public Overrides Property NullValuePrompt As String
+            Get
+                Return MyBase.NullValuePrompt
+            End Get
 
-		<DllImport("user32.dll", CharSet := CharSet.Unicode)> _
-		Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal msg As Integer, ByVal wp As IntPtr, ByVal lp As String) As IntPtr
-		End Function
-	End Class
+            Set(ByVal value As String)
+                MyBase.NullValuePrompt = value
+                ShowNullPrompt()
+            End Set
+        End Property
+
+        Public Const CustomTextEditName As String = "CustomTextEdit"
+
+        Shared Sub New()
+            Call Register()
+        End Sub
+
+        Public Sub New()
+        End Sub
+
+        Public Overrides ReadOnly Property EditorTypeName As String
+            Get
+                Return CustomTextEditName
+            End Get
+        End Property
+
+        Public Overrides Sub Assign(ByVal item As RepositoryItem)
+            BeginUpdate()
+            Try
+                MyBase.Assign(item)
+                Dim source As RepositoryItemCustomTextEdit = TryCast(item, RepositoryItemCustomTextEdit)
+                If source Is Nothing Then Return
+                NullValuePromptAlwaysVisible = source.NullValuePromptAlwaysVisible
+            Finally
+                EndUpdate()
+            End Try
+        End Sub
+
+        Public Shared Sub Register()
+            Call EditorRegistrationInfo.Default.Editors.Add(New EditorClassInfo(CustomTextEditName, GetType(MyCustomTextEdit), GetType(RepositoryItemCustomTextEdit), GetType(TextEditViewInfo), New TextEditPainter(), True))
+        End Sub
+
+        Private Sub ShowNullPrompt()
+            Dim behaviour As Integer = Convert.ToInt32(NullValuePromptAlwaysVisible)
+            If OwnerEdit IsNot Nothing AndAlso OwnerEdit.IsHandleCreated AndAlso Not String.IsNullOrEmpty(NullValuePrompt) Then
+                Call SendMessage(OwnerEdit.MaskBox.Handle, &H1501, CType(behaviour, IntPtr), NullValuePrompt)
+            End If
+        End Sub
+
+        <DllImport("user32.dll", CharSet:=CharSet.Unicode)>
+        Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal msg As Integer, ByVal wp As IntPtr, ByVal lp As String) As IntPtr
+        End Function
+    End Class
 End Namespace
